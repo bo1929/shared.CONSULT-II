cd7f7970aa84baeba00b645051679467455bf41c
bo1929 <aliosmanberk@sabanciuniv.edu>
2023-01-30 15:46:28 -0800

    Removed some redundant files.

--- scripts/consult-predict.py
+++ scripts/consult-predict.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 tax_order = ["species", "genus", "family", "order", "class", "phylum", "kingdom"]
-results = Path("./results-CONSULT/")
+results = Path("./votes-CONSULT/")
 
 for path in results.iterdir():
     with open(path) as f:
@@ -12,19 +12,18 @@ for path in results.iterdir():
     prev_pred = (None, None, None, -1, -1)
     for idx, (read, votes) in enumerate(d.items()):
         read = read.strip()
-        with open(f"./CONSULT-predictions-07/{genome}.txt", 'a') as f:
+        with open(f"./CONSULT-predictions-with_votes/{genome}.txt", 'a') as f:
             read_name, form = read.split(":")
             if len(votes)> 0:
                 max_vote = max(votes["kingdom"].values())
-                th = 0.5*max_vote/10
-                if th > 0.01:
-                    for lvl in tax_order:
-                        for pred_name, val in votes.get(lvl, {}).items():
-                            if val > th:
-                                pred = (read_name, form, pred_name, val, th)
-                                break
-                        if pred[3] > th:
+                th = max_vote/2
+                for lvl in tax_order:
+                    for pred_name, val in votes.get(lvl, {}).items():
+                        if val > th:
+                            pred = (read_name, form, pred_name, val, max_vote)
                             break
+                    if pred[3] > th:
+                        break
                 else:
                     pred = (read_name, form, "NA", 0, 0)
             else:
@@ -33,8 +32,7 @@ for path in results.iterdir():
             if idx%2  == 1:
                 if pred[4] < prev_pred[4]:
                     pred = prev_pred
-                f.write(f"{pred[0]}:{pred[1]} {pred[2]}:{pred[3]}\n")
-                # print(f"{pred[0]}:{pred[1]} {pred[2]}:{pred[3]}\n")
+                f.write(f"{pred[0]}:{pred[1]} {pred[2]}:{pred[3]}:{pred[4]}\n")
                 prev_pred = (read, None, None, -1, -1)
             else:
                 prev_pred = pred
