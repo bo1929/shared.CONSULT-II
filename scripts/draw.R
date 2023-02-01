6a4d8b51653df850ba3b77eeb5495bac56a570e9
Siavash Mirarab <smirarab@ucsd.edu>
2023-01-31 18:40:16 -0800

    update figure

--- scripts/draw.R
+++ scripts/draw.R
@@ -33,15 +33,21 @@ k=read.csv('kraken_results.csv')
 k = dcast(data=melt(k[2:9],id.vars = 1:2),formula = variable+genome~value)
 k$m= "Kraken-II"
 head(k) 
-c=read.csv('consult_results.csv')
-c = dcast(data=melt(c[2:9],id.vars = 1:2),formula = variable+genome~value)
-c$m= "CONSULT-II"
-head(c)
 cl=read.csv('CLARK-bacteria-eval.csv')
 cl = dcast(data=melt(cl[2:9],id.vars = 1:2),formula = variable+genome~value)
 head(cl)
 cl$m= "CLARK"
-b=rbind(k,c,cl)
+readcons = function(f,n) {
+  c=read.csv(f)
+  c = c[,c(1:3,6:12)]
+  c = dcast(data=melt(c[2:9],id.vars = 1:2),formula = variable+genome~value)
+  c$m = n
+  c
+}
+c01 = readcons('CONSULT-bacteria-eval-th05_d1_c001.csv','CONSULT-II (0.01)')
+c03 = readcons('CONSULT-bacteria-eval-th05_d1_c003.csv','CONSULT-II (0.03)')
+c = readcons('CONSULT-bacteria-eval-th05_d1.csv','CONSULT-II (0.00)')
+b=rbind(k,c01,c03,c,cl)
 head(b)
 
 k2 = merge(dtc[,1:13],b,by.x = "V1",by.y="genome")
@@ -68,46 +74,15 @@ ks = dcast(data=k2[,c("level","m","variable","value","V3")],
           value.var = "value",fun.aggregate = sum)
 names(ks)[1]="bin"
 ks$s = apply(ks,1,function(x) sum(as.numeric(x[4:7])))
-ks$m = factor(ks$m,levels=c("CONSULT-II", "Kraken-II","CLARK"))
+ks$m = factor(ks$m,levels=c("CONSULT-II (0.00)","CONSULT-II (0.01)","CONSULT-II (0.03)", "Kraken-II","CLARK"))
 ks
 
 #ks=ks[ks$level!="kingdom",]
 #ks$variable=factor(ks$variable,c("kingdom" ,"phylum", "class", "order" ,"family", "genus" ,"species"))
 
 
-ggplot(aes(x=bin,y=TP/(TP+FN),
-           color=m,linetype=m,shape=m),data=ks)+
-  geom_point()+
-  facet_wrap(~level)+
-  geom_line(aes(group=interaction(m)))+
-  scale_color_brewer(palette = "Dark2",name="")+
-  theme_classic()+
-  scale_shape(name="")+
-  scale_linetype(name="")+
-  theme(legend.position = "bottom" ,
-        axis.text.x = element_text(angle=90))+ # c(0.1,0.3))+
-  xlab("Distance to closest")+ylab("Recall")
-ggsave("recall.pdf",width=6.5,height = 5)
-
-
-
-
-ggplot(aes(x=bin,y=TP/(TP+FP),
-           color=m,linetype=m,shape=m),data=ks)+
-  geom_point()+
-  facet_wrap(~level)+
-  geom_line(aes(group=interaction(m)))+
-  scale_color_brewer(palette = "Dark2",name="")+
-  theme_classic()+
-  scale_shape(name="")+
-  scale_linetype(name="")+
-  theme(legend.position = "bottom" ,
-        axis.text.x = element_text(angle=90))+ # c(0.1,0.3))+
-  xlab("Distance to closest")+ylab("Precision")
-ggsave("precision.pdf",width=6.5,height = 5)
-
 ggplot(aes(x=bin,y=2*TP/(2*TP+FP+FN),
-           color=level,linetype=m,shape=m),data=ks)+
+           color=level,linetype=m,shape=m),data=ks[grepl("CONS",ks$m),])+
   geom_point(alpha=0.7)+
   facet_wrap(~floor((as.numeric(level))/2.5),
         labeller = function(x) {x[1,1]="High resolution";x[2,1]="Medium resolution";x[3,1]="Low resolution";x})+
@@ -119,29 +94,58 @@ ggplot(aes(x=bin,y=2*TP/(2*TP+FP+FN),
   theme(legend.position = "bottom" ,
         axis.text.x = element_text(angle=90,hjust=1),
         legend.box.margin = margin(0),
-        legend.margin = margin(0),
-        )+ # c(0.1,0.3))+
+        legend.margin = margin(0))+ 
   xlab("Distance to closest")+ylab("F1")
-ggsave("F1.pdf",width=7,height = 4.5)
+ggsave("F1-consults.pdf",width=7,height = 4.5)
 
 
 
-ggplot(aes(y=TP/(TP+FN),x=TP/(TP+FP),color=bin,shape=level,linetype=m),data=ks)+
-  stat_summary()+
-  stat_summary(aes(group=interaction(bin,m)),geom="line")+
+ggplot(aes(x=bin,y=2*TP/(2*TP+FP+FN),
+        color=sub(" .*","",m),shape=sub(" .*","",m),
+        linetype=sub("^$","*",gsub("[^0-9.]",'',m))),
+       data=ks)+
+  facet_wrap(~level)+
+  geom_point(alpha=0.7)+
+  geom_line(aes(group=interaction(m,level)))+
+  scale_color_brewer(palette = "Dark2",name="")+
+  theme_classic()+
+  scale_shape(name="")+
+  scale_linetype(name="")+
+  theme(legend.position = "bottom" ,
+        axis.text.x = element_text(angle=90,hjust=1),
+        legend.box.margin = margin(0),
+        legend.margin = margin(0),
+  )+ # c(0.1,0.3))+
+  xlab("Distance to closest")+ylab("F1")
+ggsave("F1-all.pdf",width=7.3,height = 5.5)
+
+ggplot(aes(y=TP/(TP+FN),x=TP/(TP+FP),
+           color=sub(" .*","",m),#shape=sub(" .*","",m),
+           shape=sub("^$","*",gsub("[^0-9.]",'',m))),
+       data=ks)+
+  facet_wrap(~level)+
+  #geom_path(aes(group=m,linetype=m),size=0.3)+
+  geom_path(aes(group=bin),color="grey50",linetype=1,alpha=0.5,size=0.3)+
+  geom_point(aes(),size=2,alpha=0.75)+
   scale_color_brewer(palette = "Dark2",name="")+
   scale_shape(name="")+
+  scale_linetype(name="")+
   theme_classic()+xlab("Precision")+ylab("Recall")+
-  theme(legend.position = "bottom",legend.direction = "horizontal")
-ggsave("precision-recall.pdf",width=7,height = 5)
+  theme(legend.position = "bottom",
+        legend.direction = "horizontal",
+        legend.box.margin = margin(0),
+        legend.margin = margin(0))+
+  guides(linetype=guide_legend(nrow=1, byrow=TRUE),
+         shape=guide_legend(nrow=1, byrow=TRUE))
+ggsave("precision-recall-methods-all.pdf",width=7,height = 5)
 
 
 ggplot(aes(y=TP/(TP+FN),x=TP/(TP+FP)),
-       data=ks)+
-  #geom_path(aes(group=m,linetype=m),size=0.3)+
-  geom_path(aes(group=bin),color="grey50",alpha=0.5,size=0.3)+
-  geom_point(aes(color=bin,shape=m),size=2,alpha=0.75)+
+       data=ks[grepl("CONS",ks$m),])+
   facet_wrap(~level)+
+  #geom_path(aes(group=m,linetype=m),size=0.3)+
+  geom_path(aes(group=bin),color="grey50",linetype=1,alpha=0.5,size=0.3)+
+  geom_point(aes(color=bin,shape=gsub("CONSULT-II ","",m)),size=2,alpha=0.75)+
   scale_color_brewer(palette = "Paired",name="")+
   scale_shape(name="")+
   scale_linetype(name="")+
@@ -152,7 +156,37 @@ ggplot(aes(y=TP/(TP+FN),x=TP/(TP+FP)),
         legend.margin = margin(0))+
   guides(linetype=guide_legend(nrow=2, byrow=TRUE),
          shape=guide_legend(nrow=2, byrow=TRUE))
-ggsave("precision-recall-methods.pdf",width=7,height = 5)
+ggsave("precision-recall-methods-consult.pdf",width=7,height = 5)
+
+
+ggplot(aes(y=TP/(TP+FN),x=TP/(TP+FP)),
+       data=ks[!grepl("0[01]",ks$m ),])+
+  facet_wrap(~level)+
+  #geom_path(aes(group=m,linetype=m),size=0.3)+
+  geom_path(aes(group=bin),color="grey50",linetype=1,alpha=0.5,size=0.3)+
+  geom_point(aes(color=bin,shape=m),size=2,alpha=0.75)+
+  scale_color_brewer(palette = "Paired",name="")+
+  scale_shape(name="")+
+  scale_linetype(name="")+
+  theme_classic()+xlab("Precision")+ylab("Recall")+
+  theme(legend.position = "bottom",
+        legend.direction = "horizontal",
+        legend.box.margin = margin(0),
+        legend.box.spacing = unit(0,"pt"),
+        legend.margin = margin(0))+
+  guides(linetype=guide_legend(nrow=2, byrow=TRUE),
+         shape=guide_legend(nrow=2, byrow=TRUE))
+ggsave("precision-recall-methods-03.pdf",width=7.3,height = 5)
+
+ggplot(aes(y=TP/(TP+FN),x=TP/(TP+FP),color=bin,shape=level,linetype=m),data=ks)+
+  stat_summary()+
+  stat_summary(aes(group=interaction(bin,m)),geom="line")+
+  scale_color_brewer(palette = "Dark2",name="")+
+  scale_shape(name="")+
+  theme_classic()+xlab("Precision")+ylab("Recall")+
+  theme(legend.position = "bottom",legend.direction = "horizontal")
+ggsave("precision-recall.pdf",width=7,height = 5)
+
 
 
 ggplot(aes(y=TP/(TP+FN),x=TP/(TP+FP)), data=ks)+
@@ -205,6 +239,7 @@ head(cvv)
 cvv = melt(cvv,id.vars = 1:5)
 cvv = cvv[cvv$variable!="superkingdom",]
 cvvp = cvv[cvv$value%in% c("TP","FP"),]
+cvvn = cvv[!cvv$value%in% c("TP","FP"),]
 #dcast(data=cvv,formula=cut(V,5)+)
 ggplot(aes(x=vote,color=variable,linetype=value),data=cvvp)+
   stat_ecdf()+
@@ -214,21 +249,60 @@ ggplot(aes(x=vote,color=variable,linetype=value),data=cvvp)+
   scale_y_continuous(name="ECDF")+
   scale_x_continuous(name="vote",trans="log10")+
   scale_linetype_manual(name="",values=c(1,3))+
-  annotate("rect", xmin = 0.003, xmax = 0.01, ymin = 0, ymax = 1, alpha = .2)+
+  annotate("rect", xmin = 0.003, xmax = 0.01, ymin = 0, ymax = 1, alpha = .1)+
+  annotate("rect", xmin = 0.003, xmax = 0.03, ymin = 0, ymax = 1, alpha = .1)+
   scale_color_brewer(palette = "Dark2",name="")
   #scale_color_manual(values=c("#AA3333","#22DD55","#DD6666","#225599"),name="")
 ggsave("votes.pdf",width=7,height=5.5)
 
-
-ggplot(aes(x=vote_n,color=value),data=cvvp)+
+ggplot(aes(x=vote,color=variable,linetype=value),data=cvvn)+
   stat_ecdf()+
-  facet_wrap(~variable)+
+  #facet_wrap(~variable)+
+  #geom_vline(xintercept = c(0.03,0.01),color="grey40",linetype=3)+
   theme_classic()+
   scale_y_continuous(name="ECDF")+
-  geom_vline(xintercept = 0.03,color="grey40")+
+  scale_x_continuous(name="vote",trans="log10")+
+  scale_linetype_manual(name="",values=c(1,3))+
+  annotate("rect", xmin = 0.003, xmax = 0.01, ymin = 0, ymax = 1, alpha = .2)+
+  scale_color_brewer(palette = "Dark2",name="")
+ggsave("votes.pdf",width=7,height=5.5)
+
+
+ggplot(aes(x=vote,color=variable,linetype=value),data=cvvp)+
+  stat_bin(data=subset(cvvp,variable=="TP"),aes(y=cumsum(after_stat(count))),geom="step")+
+  stat_bin(data=subset(cvvp,variable=="FP"),aes(y=cumsum(after_stat(count))),geom="step")+
+  theme_classic()+
+  scale_y_continuous(name="ECDF")+
+  scale_x_continuous(name="vote",trans="log10")+
+  scale_linetype_manual(name="",values=c(1,3))+
+  annotate("rect", xmin = 0.003, xmax = 0.01, ymin = 0, ymax = 1, alpha = .1)+
+  annotate("rect", xmin = 0.003, xmax = 0.03, ymin = 0, ymax = 1, alpha = .1)+
+  scale_color_brewer(palette = "Dark2",name="")
+ggsave("votes.pdf",width=7,height=5.5)
+
+
+ggplot(aes(x=log10(vote),y=vote_n),data=cvvp)+
+  facet_grid(value~variable)+
+  geom_bin_2d()+
+  theme_classic()+
+  scale_y_continuous(name="normalized vote")+
+  coord_cartesian(ylim= c(0.4995,1.001))+
+  #geom_vline(xintercept = 0.03,color="grey40")+
   scale_x_continuous(name="vote")+
-  scale_color_manual(values=c("#DD6666","#22BB55","#AA4444","#225599"),name="")
+  scale_fill_continuous(trans="log10")
 ggsave("votes-norm.pdf",width=7,height=7)
+
+
+ggplot(aes(x=vote_n,color=variable,linetype=value),data=cvvp)+
+  stat_ecdf()+facet_wrap(~value)+
+  theme_classic()+
+  scale_y_continuous(name="ECDF")+
+  scale_x_continuous(name="vote (normalized)")+
+  scale_linetype_manual(name="",values=c(1,3))+
+  scale_color_brewer(palette = "Dark2",name="")+
+  coord_cartesian(xlim= c(0.4995,1.001))+
+  theme(legend.position = "none")
+ggsave("votesnorm.pdf",width=8.8,height=5.5)
 
   e = 0.3 * diff(range(cvv$vote))
   
@@ -241,6 +315,6 @@ ggsave("votes-norm.pdf",width=7,height=7)
     stat_ecdf(colour="red", size=0.6, alpha=0.6) +
     theme_classic() +
     labs(title=paste0("adj=",adj))
-}
+
 
 
