4d96194a4cb6caf9a316ac2104cbd37d0a0315e2
bo1929 <asapci@ucsd.edu>
2023-06-28 08:01:29 -0700

    Added F1 figure for vote threshold.

--- scripts/vote_threshold_comparison.R
+++ scripts/vote_threshold_comparison.R
@@ -21,5 +21,15 @@ ggplot(scores_cc, aes(x = Precision, y = Recall, color = Distance_to_closest, sh
   theme_cowplot(font_size = 16.5) +
   scale_colour_brewer(palette = "Paired") +
   theme(aspect.ratio = 1, panel.spacing.x = unit(1.25, "lines"))
+ggsave2("../figures/vote_threshold_comparison-2.pdf")
 
-ggsave2("../figures/vote_threshold_comparison.pdf")
+ggplot(scores_cc, aes(x = Distance_to_closest, y = F1)) +
+  facet_wrap(vars(Taxonomic_Rank)) +
+  geom_line(aes(group = Method, linetype = Method), color="#d95f02") +
+  geom_point(size = 1.75, alpha = 0.85, shape="triangle", color="#d95f02") +
+  labs(shape = "Vote threshold", linetype = "Vote threshold", x = "Distance to closest", y = "F1") +
+  theme_cowplot(font_size = 17) +
+  scale_colour_brewer(palette = "Dark2") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1), aspect.ratio = 0.9) +
+  theme(panel.spacing.x = unit(1, "lines"))
+ggsave2("../figures/vote_threshold_comparison-1.pdf")
