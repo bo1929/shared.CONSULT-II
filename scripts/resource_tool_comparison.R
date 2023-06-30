66b1d06fe7e0243b79c60ded08a30f918b60a016
bo1929 <asapci@ucsd.edu>
2023-06-29 22:23:07 -0700

    Updates.

--- scripts/resource_tool_comparison.R
+++ scripts/resource_tool_comparison.R
@@ -11,9 +11,8 @@ ggplot(melt(rc[, 0:4], id.vars = 1:2)) +
   geom_col(aes(x=Tool, colour=variable, fill=variable, y=value), position = position_dodge2(padding = 0.1)) +
   theme_cowplot(font_size = 17) +
   labs(fill = "", colour = "", x = "") +
-  theme(aspect.ratio = 0.9) +
   theme(plot.margin = unit(c(0, 0, 0, 0), "cm")) +
   scale_y_continuous(name = "Memory (GB)", c(16, 32, 64, 128), sec.axis = sec_axis(~., name="Run-time (minutes)", breaks = c(5, 15, 60, 120))) +
   scale_colour_brewer(palette = "Accent", labels=c("Memory_GB" = "Memory", "Time_min"="Time")) +
   scale_fill_brewer(palette = "Accent", labels=c("Memory_GB" = "Memory", "Time_min"="Time"))
-ggsave2("../figures/resource_tool_comparison.pdf")
+ggsave2("../figures/resource_tool_comparison.pdf", width = 6.75, height = 4)
--- scripts/total_vote_impact.R
+++ scripts/total_vote_impact.R
@@ -12,7 +12,7 @@ eval0_p <- eval0[eval0$value %in% c("TP", "FP"), ]
 eval0_n <- eval0[!eval0$value %in% c("TP", "FP"), ]
 
 ggplot(aes(x = vote, color = variable, linetype = value), data = eval0_p) +
-  stat_ecdf(n = 1000) +
+  stat_ecdf(n = 1000, size = 1.25) +
   scale_y_continuous(name = "ECDF") +
   scale_x_continuous(name = TeX("Total vote ($\\bar{v}$)"), trans = "log10") +
   scale_linetype_manual(name = "", values = c(1, 3)) +
@@ -21,15 +21,15 @@ ggplot(aes(x = vote, color = variable, linetype = value), data = eval0_p) +
   scale_color_brewer(palette = "Dark2", name = "Rank", direction=-1) +
   theme_cowplot(font_size = 17) +
   theme(aspect.ratio = 1)
-ggsave2("../figures/total_vote_impact.pdf")
+ggsave2("../figures/total_vote_impact.pdf", width = 8, height = 6)
 
 ggplot(aes(x = voteNormalized, color = variable), data = eval0_p) +
-  stat_ecdf(n = 1000) +
+  stat_ecdf(n = 1000, size = 0.75) +
   scale_y_continuous(name = "ECDF") +
   facet_wrap(vars(value)) +
   scale_x_continuous(name = "Total vote (normalized)", trans = "log10") +
   scale_color_brewer(palette = "Dark2", name = "Rank", direction=-1) +
   theme_cowplot(font_size = 17) +
-  theme(legend.position="none")
+  theme(legend.position="none") +
   theme(aspect.ratio = 1.25)
-ggsave2("../figures/total_vote_impact.pdf")
+ggsave2("../figures/total_nvote_impact.pdf", width = 8, height = 6)
