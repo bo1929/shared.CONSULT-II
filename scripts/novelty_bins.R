61c2f37b88a5b9ca32808c0771a101f9e09c1e53
bo1929 <asapci@ucsd.edu>
2023-07-03 06:37:04 -0700

    Updates.

--- scripts/novelty_bins.R
+++ scripts/novelty_bins.R
@@ -19,6 +19,6 @@ ggplot(d2c, aes(x = name, y = X3, color = X3)) +
   geom_hline(aes(yintercept = X3, color = X3), bb, linewidth = 1, alpha = 0.5) +
   theme_cowplot(font_size = 15) +
   labs(y = "Distance to the closest reference genome", x = "Genome", color = "") +
-  theme(axis.text.x = element_text(angle = 90, size = 4), aspect.ratio = 0.65)
+  theme(axis.text.x = element_text(angle = 90, size = 5), aspect.ratio = 0.45)
   # theme(axis.text.x = element_blank(), aspect.ratio=0.65)
-ggsave2("../figures/novelty_bins-wlabels.pdf")
+ggsave2("../figures/novelty_bins-wlabels.pdf", width = 11, height = 6)
